2ac18b73d8716a265a7b615a697f3bfa1d371ebc
hzw020222-bit <hzw020222@gmail.com>
2026-03-11 10:37:58 +0800

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -4,21 +4,21 @@ export const productsData = [
     id: "1", 
     name: "精品1", 
     price: "$580.00", 
-    image: "/public/微信图片_20260310193212_9_21.jpg",
+    image: "/微信图片_20260310193212_9_21.jpg",
     description: "采用纯铜打造，手工雕刻龙纹，极具收藏价值。"
   },
   { 
     id: "2", 
     name: "精品2", 
     price: "$120.00", 
-    image: "/public/微信图片_20260310193212_9_21.jpg",
+    image: "/微信图片_20260310193212_9_21.jpg",
     description: "高清防尘亚克力材质，带LED底座灯光。"
   },
   { 
     id: "3", 
     name: "精品3", 
     price: "$45.00", 
-    image: "/public/微信图片_20260310193212_9_21.jpg",
+    image: "/微信图片_20260310193212_9_21.jpg",
     description: "进口胡桃木，手工打磨，沉稳大气。"
   }
 ];
