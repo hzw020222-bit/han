bde9203f9f105356ed0b952e78d931626d6170b1
hzw020222-bit <hzw020222@gmail.com>
2026-04-16 15:18:23 +0800

    Update page.tsx

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { use } from 'react';        // ← 新增这一行
 
 const products = [
